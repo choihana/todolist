a9a4f2fa71cbfb24518c8502b4dd8024dcab625c
Joey <hanazzang999@gmail.com>
2023-08-11 14:14:21 +0900

    update, delete view

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 from django.urls import reverse_lazy
 
@@ -10,7 +10,6 @@ from django.urls import reverse_lazy
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
-
 
 class TaskDetail(DetailView):
     model = Task
@@ -20,4 +19,14 @@ class TaskCreate(CreateView):
     model = Task
     fields = ['user','title','description', 'complete']
     # 아이템 생성 성공 시, 리스트 화면으로 이동
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['user', 'title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
